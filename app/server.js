bacea8c003e82a95be864001bf3a4da92c537746
Aditya Kumar Sharma <aditya@Adityas-MacBook-Pro.local>
2024-12-13 04:05:27 +0530

    Updating branch

--- app/server.js
+++ app/server.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // Import the Express library
+const app = express(); // Create an instance of an Express application
 
+// Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!'); // Send a response to the client
 });
 
+// Start the server and listen on port 3000
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
